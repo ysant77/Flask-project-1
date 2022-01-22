320c2a4f14f97e7a9936573e9ff05273ca4df278
ysant77 <ysant77@gmail.com>
2022-01-22 15:07:09 +0530

    Added templates and blueprint

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 from distutils.debug import DEBUG
 
 
+import os
+
 class Config(object):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 10
     #SQLALCHEMY_ECHO = True
     #This above line will print all SQL queries executed as part of ORM
 
 class ProdConfig(Config):
-    pass
+    SECRET_KEY = os.urandom(24)
 
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SECRET_KEY = os.urandom(24)
